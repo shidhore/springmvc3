5747860a989b60b43f1e40be4cea87309ae9ede9
Mandar.Shidhore <mandar.shidhore@travelport.com>
2018-05-06 12:43:30 -0600

    Content Negotiation - json Example

--- src/main/java/com/ms/controller/MinutesController.java
+++ src/main/java/com/ms/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.ms.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ms.model.Activity;
 import com.ms.model.Exercise;
 
 @Controller
@@ -22,6 +28,19 @@ public class MinutesController {
 		System.out.println("==== addMinutes ====");
 		System.out.println(">> exercise minutes = " + exercise.getMinutes());
 		return "addMinutes";
+	}
+
+	// Method for returning view as json; ties to content negotiation section inside servlet-config.xml
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+		List<Activity> activities = new ArrayList<>();
+		Activity run = new Activity();
+		run.setDescription("Run");
+		activities.add(run);
+		Activity bike = new Activity();
+		bike.setDescription("Bike");
+		activities.add(bike);
+		return activities;
 	}
 
 }
---
+++ src/main/java/com/ms/model/Activity.java
@@ -0,0 +1,15 @@
+package com.ms.model;
+
+public class Activity {
+	// tied to minutes in addMinutes.jsp
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
